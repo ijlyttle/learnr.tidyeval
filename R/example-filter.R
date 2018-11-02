8fd02bcd0fd6a555ee3785f6b38a8707a7b3d1aa
Ian Lyttle <ian.lyttle@schneider-electric.com>
2018-11-01 22:08:51 -0500

    updates

--- R/example-filter.R
+++ R/example-filter.R
@@ -1,14 +1,14 @@
 #' filter on name and value
 #'
-#' @param .data      `data.frame` to be filtered
-#' @param var_name   `character`, name of column within `.data` to filter
-#' @param value      object,
+#' @param data          `data.frame` to be filtered
+#' @param input_name    `character`, name of column within `.data` to filter
+#' @param input_value  object
 #'
-#' @return filtered copy of `.data`
+#' @return filtered copy of `data`
 #' @examples
-#' \dontrun{ex_filter_name_value(mtcars, "cyl", 6)}
+#'  ex_filter_name_value(mtcars, "cyl", 6)
 #' @export
 #'
-ex_filter_name_value <- function(.data, var_name, value) {
-  dplyr::filter(.data, !!sym(var_name) == !!value)
+ex_filter_name_value <- function(data, input_name, input_value) {
+  dplyr::filter(data, !!sym(input_name) == !!input_value)
 }
